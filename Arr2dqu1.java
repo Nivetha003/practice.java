f28472a0d6f353f9470f2f2847e9bb44400ded53
Nivetha <niviragu2k5@gmail.com>
2026-02-24 10:26:30 +0530

    Add files via upload

---
+++ Arr2dqu1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Arr2dqu1
+{
+    public static int sum(int arr[][],int row,int col){
+        int sum = 0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                sum = sum+arr[i][j];
+            }
+        }
+        return sum;
+    }
+    	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int row = sc.nextInt();
+	    int col = sc.nextInt();
+	    int [][]arr =  new int[row][col];
+	    for(int i=0;i<row;i++){
+	        for(int j=0;j<col;j++){
+	            arr[i][j] = sc.nextInt();
+	        }
+	    }
+	    
+		System.out.println(sum(arr,row,col));
+	}
+}
